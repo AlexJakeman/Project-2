006a32692acf9671cd6ef880275eee3e6424057b
AlexJakeman <Alexjakeman4@gmail.com>
2023-11-03 03:41:30 +0000

    adjusted eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
-    },
+        "commonjs": true,
+        "es6": true,
+        "jquery": true
+      },
     "extends": "eslint:recommended",
     "overrides": [
         {
